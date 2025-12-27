e0bdb2ff384385e1087e482e47df717f2555d434
Alok Illur <alokillur99@gmail.com>
2025-12-27 10:43:05 +0530

    feat: Implement client-side routing with protected routes, a login route, and a 404 Not Found page.

---
+++ Client/src/pages/notFound/NotFound.jsx
@@ -0,0 +1,23 @@
+import "./NotFound.css";
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="not-found-title">404</h1>
+        <h2 className="not-found-subtitle">Oops! Page Not Found</h2>
+        <p className="not-found-message">
+          The page you are looking for might have been removed, had its name
+          changed, or is temporarily unavailable.
+        </p>
+        <button onClick={() => navigate("/")} className="not-found-button">
+          Go to homepage
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
