20ce511002174ec9ab6a41279eaa7f30899efade
Alok Illur <alokillur99@gmail.com>
2025-12-27 11:26:18 +0530

    feat: Add `Category` and `DisplayCategory` components for rendering and selecting categories with associated assets.

--- Client/src/components/displayCategory/DisplayCategory.jsx
+++ Client/src/components/displayCategory/DisplayCategory.jsx
@@ -1,20 +1,48 @@
-import Category from '../category/Category';
-import './DisplayCategory.css';
+import Category from "../category/Category";
+import "./DisplayCategory.css";
+import { assets } from "../../assets/assets";
 
-function DisplayCategory({categories, selectedCategory, setSelectedCategory}) {
+function DisplayCategory({
+  categories,
+  selectedCategory,
+  setSelectedCategory,
+}) {
+  const allItemsCategory = {
+    name: "All Items",
+    imgUrl: assets.device,
+    items: categories.reduce((acc, cat) => acc + (cat.items || 0), 0),
+  };
+
   return (
     <div>
-      <div className="row g-2" style={{width:"100%" , margin:0}}>
+      <div className="row g-3" style={{ width: "100%", margin: 0 }}>
+        <div
+          key={"all"}
+          className="col-xxl-4 col-xl-6 col-lg-12 col-md-12 col-sm-12"
+          style={{ padding: "0 10px" }}
+        >
+          <Category
+            category={allItemsCategory}
+            isSelected={selectedCategory === ""}
+            onclick={() => setSelectedCategory("")}
+          />
+        </div>
         {categories.map((category) => (
-          <div key={category.categoryId} className="col-xxl-4 col-xl-6 col-lg-12 col-md-12 col-sm-12" style={{padding: '0 10px'}}>
-            <Category category={category} 
-            isSelected={selectedCategory === category.categoryId} 
-            onclick={() => setSelectedCategory(category.categoryId)}/>
+          <div
+            key={category.categoryId}
+            className="col-xxl-4 col-xl-6 col-lg-12 col-md-12 col-sm-12"
+            style={{ padding: "0 10px" }}
+          >
+            <Category
+              category={category}
+              isSelected={selectedCategory === category.categoryId}
+              onclick={() => setSelectedCategory(category.categoryId)}
+            />
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default DisplayCategory;
